c0f524ccd31c251d80bfb4a156e1a0a1e0074787
RedContritio <RedContritio@qq.com>
2020-02-15 20:14:21 +0800

    Initialize
    基本框架

---
+++ Event.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventEditor
+{
+    using SideEffect = String;
+    public class Event
+    {
+        public int ID;
+        
+        public string Note;// 备注
+
+        public string PictureID; // 图片 ID
+
+        public string AvatarID; // 头像 ID
+
+        public string UIText; // UI 文本
+
+        public int[] Parameters; // 参数列表
+
+        public int[] Options; // 选项列表
+
+        public SideEffect[] Requirements; // 需求
+
+        public string JumpTo; // 跳转
+
+        public SideEffect[] Effects; // 事件结果
+
+        public string UnknownData1;
+
+        public string UnknownData2;
+
+        public int TextBox; // 启用文本框
+
+        public override string ToString()
+        {
+            return ID.ToString() + "," + Note + "," + PictureID + "," + AvatarID + "," + UIText + "," +
+                (Parameters == null ? "" : string.Join("|", Parameters)) + "," +
+                (Options == null ? "" :  string.Join("|", Options)) + "," +
+                string.Join("|", Requirements) + "," +
+                JumpTo + "," + string.Join("|", Effects) + "," + UnknownData1 + "," + UnknownData2 + "," + TextBox.ToString();
+        }
+
+        public void ShowBasicInfo()
+        {
+            Console.Write(IDInfo);
+            if (NoteInfo != null) Console.Write("\t" + NoteInfo);
+            if (PictureIDInfo != null) Console.Write("\t" + PictureIDInfo);
+            if (AvatarIDInfo() != null) Console.Write("\t" + AvatarIDInfo());
+            if (ParametersInfo() != null) Console.Write("\t" + ParametersInfo());
+            Console.WriteLine();
+            bool needNewLine = false;
+            if(TextBox != 0)
+            {
+                Console.Write("含有文本框" + "\t");
+                needNewLine = true;
+            }
+            if (EffectsInfo() != null)
+            {
+                Console.Write(EffectsInfo() + "\t");
+                needNewLine = true;
+            }
+            if(needNewLine) Console.WriteLine();
+            if(UIText.Length > 0)
+            {
+                Console.WriteLine(UITextInfo);
+            }
+        }
+
+        public string MinForm => ID + "\t" +
+            ((RequirementsInfo() != null) ? (RequirementsInfo() + "\t") : "") +
+            (UIText.Length > 0 ? UIText : Note);
+
+        public string ShortForm(int length)
+        {
+            string s = MinForm;
+            if(length < s.Length)
+                return s.Substring(0, length);
+            else
+                return s;
+        }
+
+        public string IDInfo => "事件ID: " + ID.ToString();
+        public string NoteInfo => (Note.Length > 0) ? "备注：" + Note : null;
+        public string PictureIDInfo => (PictureID.Length > 0) ? "照片ID：" + PictureID : null;
+        public string UITextInfo => "UI 文本：\t" + UIText;
+
+        public string JumpInfo => (int.TryParse(JumpTo, out int target) ? (((target > 0) ? "跳转至" : "特殊跳转") + target) : null);
+
+        public int JumpToId => int.Parse(JumpTo);
+
+        public string AvatarIDInfo()
+        {
+            if(int.TryParse(AvatarID, out int id))
+            {
+                if(id == 0)
+                    return "头像：动态";
+                else if(id == -1)
+                    return "头像：太吾";
+                else if(id == -99)
+                    return "头像：无";
+                else
+                    return "头像ID：" + AvatarID;
+            }
+            return null;
+        }
+
+        public string ParameterInfo(int param)
+        {
+            string[] results = {
+                "", "角色ID", "道具ID", "功法ID", "资源ID",
+                "道具ID", "baseSkillID", "数值", "skillID", "好感等级",
+                "未知", "partWorldMap", "角色ID", "gangValueId"};
+
+            if(param >= 0 && param < results.Length)
+            {
+                return "<" + param + ">" + " " + results[param];
+            }
+            return "<" + param + ">" + " " + "未知参数";
+        }
+        public string ParametersInfo()
+        {
+            if(Parameters != null && Parameters.Length > 0)
+            {
+                string result = "参数列表：" + ParameterInfo(Parameters[0]);
+                for(int i = 1; i < Parameters.Length; ++i)
+                {
+                    result += "，" + ParameterInfo(Parameters[i]);
+                }
+                return result;
+            }
+            else
+                return null;
+        }
+
+        public string RequirementsInfo()
+        {
+            if(Requirements != null && Requirements.Length > 0 && Requirements[0].Length > 0)
+            {
+                return "需求：" + string.Join(" | ", Requirements);
+            }
+            else
+                return null;
+        }
+        public string EffectsInfo()
+        {
+            if (Effects != null && Effects.Length > 0 && Effects[0].Length > 0)
+            {
+                return "触发结果：" + string.Join(" | ", Effects);
+            }
+            else
+                return null;
+        }
+    }
+
+    public static class ExpandMethod
+    {
+        public static Event ToEvent(this string[] eventdata)
+        {
+            Event e = new Event();
+            int index = 0;
+            e.ID = int.Parse(eventdata[index++]); // 0
+            e.Note = eventdata[index++]; // 1
+            e.PictureID = eventdata[index++]; // 2
+            e.AvatarID = eventdata[index++]; // 3
+            e.UIText = eventdata[index++]; // 4
+
+            string[] parameterStrs = eventdata[index++].Split("|"); // 5
+            if (parameterStrs.Length == 0 || parameterStrs[0].Length == 0)
+                e.Parameters = null;
+            else
+            {
+                e.Parameters = new int[parameterStrs.Length];
+                for(int i = 0; i < parameterStrs.Length; ++i)
+                {
+                    e.Parameters[i] = int.Parse(parameterStrs[i]);
+                }
+            }
+
+            string[] optionStrs = eventdata[index++].Split("|"); // 6
+            if(optionStrs.Length == 0 || optionStrs[0].Length == 0)
+                e.Options = null;
+            else
+            {
+                e.Options = new int[optionStrs.Length];
+                for (int i = 0; i < optionStrs.Length; ++i)
+                {
+                    e.Options[i] = int.Parse(optionStrs[i]);
+                }
+            }
+
+            e.Requirements = eventdata[index++].Split("|"); // 7
+
+            e.JumpTo = eventdata[index++]; // 8
+            
+            e.Effects = eventdata[index++].Split("|"); // 9
+
+            e.UnknownData1 = eventdata[index++]; // 10
+
+            e.UnknownData2 = eventdata[index++]; // 11
+
+            e.TextBox = int.Parse(eventdata[index++]); // 12
+
+            return e;
+        }
+
+        public static void ShowEvent(this Dictionary<int, Event> EventDict, int id)
+        {
+            Event e = EventDict[id];
+            e.ShowBasicInfo();
+            if(e.Options != null && e.Options.Length > 0)
+            {
+                Console.WriteLine("可选选项：");
+                for(int i = 0; i < e.Options.Length; ++i)
+                {
+                    // 故意起名 num1
+                    int num1 = e.Options[i];
+                    if(EventDict.ContainsKey(num1))
+                    {
+                        Console.WriteLine(EventDict[num1].MinForm);
+                    }
+                    else
+                    {
+                        Console.WriteLine(num1 + "\t" + "未知事件");
+                    }
+                }
+            }
+            if(e.JumpInfo != null)
+            {
+                Console.WriteLine(e.JumpInfo + ((e.JumpToId > 0) ? "\n" + EventDict[e.JumpToId].MinForm : ""));
+            }
+        }
+
+        public static int[] Append(this Dictionary<int, Event> EventDict, Dictionary<int, Event> patch)
+        {
+            int[] cnts = new int[2] { 0, 0};
+            foreach(int key in patch.Keys)
+            {
+                if(EventDict.ContainsKey(key))
+                {
+                    EventDict[key] = patch[key];
+                    ++cnts[0];
+                }
+                else
+                {
+                    EventDict.Add(key, patch[key]);
+                    ++cnts[1];
+                }
+            }
+            return cnts;
+        }
+    }
+}
---
+++ EventSL.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace EventEditor
+{
+    public static class EventSL
+    {
+        public enum FILE_STATE {
+            SUCCESS,
+            NOT_EXIST,
+            ILLEGAL
+        };
+        static string eventFileHead = "#,0,1,2,3,4,5,6,7,8,9,10,11";
+        public static FILE_STATE isEventFile(string fileName)
+        {
+            if(!File.Exists(fileName)) return FILE_STATE.NOT_EXIST;
+            FileStream fs = File.Open(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string head = sr.ReadLine();
+
+            sr.Close();
+            fs.Close();
+
+            if (head != null && head.Equals(eventFileHead))
+            {
+                return FILE_STATE.SUCCESS;
+            }
+            else return FILE_STATE.ILLEGAL;
+        }
+        private static void Debug(string info, int level = 0)
+        {
+            Console.WriteLine(info);
+        }
+
+        public static Dictionary<int, Event> loadEventFromFile(string fileName)
+        {
+            if(isEventFile(fileName) != FILE_STATE.SUCCESS) return new Dictionary<int, Event>();
+            StreamReader istream = new StreamReader(fileName);
+            istream.ReadLine(); // jump head line
+
+            Dictionary<int, Event> events = new Dictionary<int, Event>();
+
+            string eventline;
+            string[] eventdata;
+            while(!istream.EndOfStream)
+            {
+                eventline = istream.ReadLine().Trim();
+                if(eventline != null && eventline.Length > 0)
+                {
+                    eventdata = eventline.Split(',');
+                    
+
+                    if(eventdata.Length >= 13)
+                    {
+                        if(int.TryParse(eventdata[0], out int id))
+                        {
+                            events.Add(id, eventdata.ToEvent());
+                        }
+                        else
+                        {
+                            Debug("不合法数据：\"" + eventline + "\"，失败原因：不合法 ID");
+                        }
+                    }
+                    else
+                    {
+                        Debug("不合法数据：\"" + eventline + "\"，失败原因：项数过少（需要 13，只有");
+                    }
+                }
+            }
+
+            istream.Close();
+            return events;
+        }
+    }
+}
